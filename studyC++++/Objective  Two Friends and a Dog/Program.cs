df6a812ba237efb523b325cfc3f8f9f50d043069
Ursuzev Boris <BorerKicker@yandex.ru>
2022-06-05 19:28:47 +0300

    Task about friends and dog

---
+++ studyC++++/Objective  Two Friends and a Dog/Program.cs
@@ -0,0 +1,25 @@
+﻿int speedFirstFriend = 2;
+int speedSecondFriend = 3;
+int speedDog = 6;
+int counter = 0;
+double distance = 3000;
+int limitDistance = 50;
+bool friends = true;
+while(distance > limitDistance)
+{
+    double time = 0;
+    if(friends == true)
+    {
+        time = distance / (speedSecondFriend + speedDog);
+        distance = distance - time * (speedFirstFriend + speedSecondFriend);
+        friends = false;
+    }
+    else
+    {
+        time = distance / (speedFirstFriend + speedDog);
+        distance = distance - time * (speedFirstFriend + speedSecondFriend);
+        friends = true;
+    }
+    counter++;
+}
+Console.WriteLine(counter);
