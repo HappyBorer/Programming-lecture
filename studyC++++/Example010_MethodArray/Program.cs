82d5a7303bdb3dda6a42fc6920b91fa46bb42dda
Ursuzev Boris <BorerKicker@yandex.ru>
2022-06-08 14:12:45 +0300

    Added example intro method, intro array, method array, array library

---
+++ studyC++++/Example010_MethodArray/Program.cs
@@ -0,0 +1,15 @@
+﻿int [] array = {2, 5, 3, 6, 8, 1, 4};
+
+int n = array.Length;
+int find = 4;
+
+int index = 0;
+while (index < n)
+{
+    if (array[index] == find)
+    {
+        Console.WriteLine(index);
+        break;
+    }
+    index++;
+}
