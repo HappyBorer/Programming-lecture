a13f54a7160b89d5499d58fc90d67b3f94290e24
Ursuzev Boris <BorerKicker@yandex.ru>
2022-06-30 19:27:20 +0300

    added my introduction

---
+++ studyC++++/my introduction/Program.cs
@@ -0,0 +1,21 @@
+﻿/*bool IsEven(int value)
+{
+ return value % 2 == 0;
+}*/
+// Это можно записать так
+// bool IsEven(int value) => value % 2 == 0;
+// или так
+// var IsEven = (int v) => v % 2 == 0;
+
+Random r = new Random();
+
+Console.CursorVisible = false;
+while(true)
+{
+    Console.SetCursorPosition(
+        left: r.Next(Console.WindowWidth),
+        top: r.Next(Console.WindowHeight)
+    );
+    Console.Write(r.Next(10));
+    Thread.Sleep(1000);
+}
